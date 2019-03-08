ad0f3e359f4648bd8d8a7b44b1a410e2fec8f36f
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2019-03-08 16:13:07 +0530

    Allow nodes to access neighbor states, integrate with simulator.

--- envRLS.py
+++ envRLS.py
@@ -5,6 +5,7 @@ from p2pnetwork import P2PRandomNetwork
 from envSimpleP2P import P2PGroup
 from envSimple import SimpleEnvironment
 from simulator import Simulator
+from agent import Agent, SegmentRequest
 
 import numpy as np
 import load_trace
@@ -26,14 +27,16 @@ class SingleAgentEnv():
     def __init__(self, vi, traces, simulator, nodeId, abr=None):
         # the catch here is that the traces we have is a map of neighbor node link to the cooked trace
         self.cookedTime, self.cookedBW, self.traceFile = {}, {}, {}
+        
+        # this is the bandwidth pointer which points to a random timestep in the trace which is assumed to be the starting point for our node's bandwidth 
         self.lastBandwidthPtr = {}
 
         for node in traces:
             self.cookedTime[node], self.cookedBW, self.traceFile = traces[node]
-            self.lastBandwidthPtr[node] =  int(np.random.uniform(1, len(self._vCookedTime[node])))
+            self.lastBandwidthPtr[node] =  int(np.random.uniform(1, len(self.cookedTime[node])))
 
        # the agent that the environment takes care of
-        self.agent = agent
+        self.agent = Agent(vi, self, abr)
         
         self.simulator = simulator
         self.isDead = False
@@ -43,6 +46,7 @@ class SingleAgentEnv():
         
         # neighbors
         self.peers = traces.keys()
+        self.neighbor_envs = {}
 
         # check if it is a super peer neighbor i.e neighbor of peer id 0
         self.isSuperPeerNeighbor = SUPERPEER_ID in self.peers
@@ -58,12 +62,13 @@ class SingleAgentEnv():
         self.collectedChunks = {}
         # if it is the super peer, all the chunks of all qualities are present
         if nodeId == SUPERPEER_ID:
-            init_superpeer()
+            self.init_superpeer()
     
     '''Performs initialization required for a super peer
     Invoked only when nodeId = SUPERPEER_ID
     '''
-    def init_superpeer():
+    def init_superpeer(self):
+        print("Init superpeer")
         for quality in range(len(self.videoInfo.fileSizes)):
             self.collectedChunks[quality] = {}
             for segid in range(len(self.videoInfo.fileSizes[quality])):
@@ -72,8 +77,12 @@ class SingleAgentEnv():
                     duration = self.videoInfo.duration - segid*self.videoInfo.segmentDuration
                 else:
                     duration = self.videoInfo.segmentDuration
-                self.collectedChunks[quality][segid] = SegmentRequest(quality, 0, 0, duration, segId, self.videoInfo.fileSizes[quality][segid], self)
+                self.collectedChunks[quality][segid] = SegmentRequest(quality, 0, 0, duration, segid, self.videoInfo.fileSizes[quality][segid], self)
 
+    def setNeighbors(self, neighbor_envs):
+        # neighbor_envs is a mapping between the neighbor id to the corresponding env object
+        self.neighbor_envs = neighbor_envs
+    
     def getNow(self):
         return self.simulator.getNow()
 
@@ -88,11 +97,21 @@ class SingleAgentEnv():
         self.agent._rFinish()
         self.hasFinished = True
 
+    '''
+    Gets the bandwidth of the link between the node and the given neighbor
+    '''
+    def getBandwidth(self, neighbor):
+        pass    
+
     def start(self, startedAt=-1):
         if not self.agent:
             raise Exception("Node agent to start")
         self.lastDownloadedAt = self.getNow()
-        self.agent.start(startedAt)
+        print("Agent starting: "+ str(self.nodeId))
+        
+        # the super peer doesn't need to download
+        if self.nodeId != SUPERPEER_ID: 
+            self.agent.start(startedAt)
 
 
     '''
@@ -110,41 +129,46 @@ class SingleAgentEnv():
         2. Among all those that have it, use the one with best throughput at the moment
         3. if none have it, use the super peer.
         '''
-        candidates = []
+        
+        candidates = np.array([])
         for neighbor in self.peers:
-            if segId in neighbor.collectedChunks:
+            if segId in self.neighbor_envs[neighbor].collectedChunks:
                candidates.append(neighbor)
         
         if len(candidates) == 0:
             # fetch from superpeer
             return SUPERPEER_ID
         
-        for node in candidates:
-            # CONTINUE HERE NEXT
-            # TODO: get the best candidate   
-            pass
+        # CONTINUE HERE NEXT
+        # TODO: get the best candidate   
+        # the bandwidth changes cannot be predicted by the node throughout time, so it takes into consideration the present bandwidth of the link and calculates throughput accordingly
+
+        print("Log: Sim now: %s" % self.getNow())
+        return np.random.choice(candidates) 
 
 
     '''
     This is the entry point which the agent calls for downloading the data
     '''
     def _rDownloadNextData(self, nextSegId, nextQuality, sleepTime):
-        if self.isDead: return
-        
+        print("here")
+        if self.isDead:
+           return
         # TODO: The ABR policy is delegated to the agent at present. We must add logic to handle P2P fetching
-        
-        neighborToFetchFrom = self.getPeerToFetchFrom()
-        self._rFetchSegment(nextSegId, nextQuality, sleepTime)
+        neighborToFetchFrom = self.getPeerToFetchFrom(nextSegId)
+        print(neighborToFetchFrom)
 
- 
+        #self._rFetchSegment(nextSegId, nextQuality, sleepTime)
+
 
 def setupEnv(traces, vi, network, abr=None):
-   simulator = Simulator()
-   agents = []
-   trace_map = {}
+    simulator = Simulator()
+    agents = []
+    trace_map = {}
+    envs = {}
 
-   # allocate different traces for each link in the network
-   for x, nodeId in enumerate(network.nodes()):
+    # allocate different traces for each link in the network
+    for x, nodeId in enumerate(network.nodes()):
         link_traces = {}
         for neighbor in network.grp.neighbors(nodeId):
            edge = (min(neighbor, nodeId), max(neighbor, nodeId))
@@ -154,8 +178,16 @@ def setupEnv(traces, vi, network, abr=None):
                trace_map[edge] = traces[idx]
            else:
                link_traces[neighbor] = trace_map[edge]
-       # TODO: make an environment
-   
+        # TODO: make an environment
+        env = SingleAgentEnv(vi, link_traces, simulator, nodeId)
+        envs[nodeId] = env
+        print("Starting node %d" % nodeId)
+
+    for x, nodeId in enumerate(network.nodes()):
+        neighbor_envs = {neighbor: envs[neighbor] for neighbor in network.grp.neighbors(nodeId)}
+        envs[nodeId].setNeighbors(neighbor_envs)
+        simulator.runAt(101.0 + x, envs[nodeId].start, 5)
+    simulator.run()
 
 def main():
     network = P2PRandomNetwork(6)
