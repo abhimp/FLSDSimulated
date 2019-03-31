e8c36e7f2620c0a319e558adc0393fa6a280a7fb
Abhijit Mondal <abhijit.manpur@gmail.com>
2019-03-31 23:14:58 +0530

    update

--- abrPensiev.py
+++ abrPensiev.py
@@ -41,7 +41,7 @@ SUMMARY_DIR = './results'
 LOG_FILE = './results/log'
 # in format of time_stamp bit_rate buffer_size rebuffer_time video_chunk_size download_time reward
 NN_MODEL = None
-NN_MODEL = './model/pretrain_linear_reward.ckpt'
+NN_MODEL = './model/nn_model_ep_89600.ckpt'
 
 # CHUNK_COMBO_OPTIONS = []
 SETUP_ABR_CALL_COUNTER = 0
